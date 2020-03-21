e0229500fc7d87f260970485de0aac5e6223d98e
m-barrera <Mariano.Barrera@live.de>
2020-03-21 23:59:27 +0100

    resolver and schema for profile & querys

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,9 @@
 import { User } from './models/user'
+import { Profiles } from './models/profiles'
 import jwt from  'jsonwebtoken'
 import bcrypt from 'bcryptjs'
+import uid from 'uid'
+
 import context from './middleware/context'
 
 const secret_key = "0tymjDYWji"
@@ -26,11 +29,14 @@ export const resolvers = {
             );
             return { userId: user.id, token: token, tokenExpiration: 1 }
           },
-        hello: (args, req, context) => {
+        getMyProfiles: async (args, req, context) => {
             if(!context.isAuth){
-                throw Error("unauth")
+                throw Error("You need to be authenticated to get your profiles")
             }
-            return "hello"
+            const profilelist = Profiles.find({uid:context.userId})
+            
+            return profilelist
+           
         }
     },
     Mutation:{
@@ -60,6 +66,43 @@ export const resolvers = {
                 }
               );
               return { userId: registereduser.id, token: token, tokenExpiration: 1 }
-        },        
+        },
+        createProfile: async (args, req, context,{vorname,nachname,strasse,hausnummer,adresszusatz,stadt,plz,land,long,lat}) => {
+
+            if(!context.isAuth){
+                throw Error("Please authenticate to add a profile!")
+            }
+
+            const puid = uid(16)
+
+            const profile = new Profiles({
+                uid: context.userId ,
+                pid: puid,
+                vorname:req.vorname,
+                nachname:req.nachname,
+                strasse:req.strasse,
+                hausnummer:req.hausnummer,
+                adresszusatz:req.adresszusatz,
+                stadt:req.stadt,
+                plz:req.plz,
+                land:req.land,
+                long:req.long,
+                lat:req.lat,
+            })
+
+            const saveprofile = await profile.save()
+            return saveprofile
+        },
+        deleteProfile:async(args, req, context) => {
+            if(!context.isAuth){
+                throw Error("Please authenticate to remove a profile!")
+            }
+        try{
+            await Profiles.deleteOne({uid:context.userId, pid: req.pid})
+        }catch(err){
+            throw err
+        }
+        return true
+        }        
     }
 }   
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -17,12 +17,29 @@ export const typeDefs = gql`
         tokenExpiration: Int!
     }
 
+    type Profiles{
+        uid: String!
+        pid: String!
+        vorname: String!
+        nachname: String!
+        strasse: String!
+        hausnummer: String!
+        adresszusatz: String!
+        stadt: String!
+        plz: String!
+        land: String!
+        long: Float!
+        lat: Float!
+    }
+    
     type Query{
-        hello:String!
-        login(email: String!, password: String!):LoginResponse!    
+        login(email: String!, password: String!):LoginResponse!
+        getMyProfiles:[Profiles!]!  
     }
 
     type Mutation{
         register(vorname: String!, nachname:String!, email: String! password: String!):LoginResponse!
+        createProfile(vorname: String!, nachname: String!, strasse: String!, hausnummer: String!, adresszusatz: String!, stadt: String!, plz: String!, land: String!, long: Float!, lat: Float!):Profiles!
+        deleteProfile(pid:String!):Boolean!
     }
 `
